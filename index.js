5f1e06370f5ab657c9c268c3cfd5a85bb9c484f4
Ryan Anderson <ryanaanderson0@gmail.com>
2020-11-02 10:16:37 -0800

    added PORT variable initialized to port 5000 for development. used app.listen to run the web server on port 5000. Also added response to app.listen that console.logs a message

---
+++ index.js
@@ -0,0 +1,28 @@
+import Express from "express"; //now able to use ES6 to import Express, instead of using require
+
+const app = Express(); //initializing app that contains express function
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); //runs the web server
+
+
+
+/* 4 HTTP Functions --
+
+GET , PUT , POST , DELETE 
+
+app.get( '/', function(req, res) {
+    // Fetch from database
+    // Load pages
+    // Return JSON
+    // FUll access to request and response
+
+});
+
+app.put()
+app.post
+app.delete
+
+
+*/ 
